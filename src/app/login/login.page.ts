a4336fc4e0ab0603d97ee6034f00eaa4b2cd6931
Haniel <hanielbarrosf@gmail.com>
2022-04-06 20:58:46 -0300

    Adding if else to check password/user and removing a useless endpoint

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 import { environment } from 'src/environments/environment';
 import { User } from './user.interface';
 
@@ -14,10 +15,24 @@ export class LoginPage implements OnInit {
   form: FormGroup;
   user: User
 
-  constructor(private http: HttpClient, private formBuilder: FormBuilder, private router: Router,) { }
+  constructor(private http: HttpClient, private formBuilder: FormBuilder, private router: Router, private alertController: AlertController) { }
 
   ngOnInit() {
     this.buildForm();
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Algo de errado não está certo',
+      message: 'Usuário ou senha inválidos.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
   }
 
   buildForm() {
@@ -31,24 +46,18 @@ export class LoginPage implements OnInit {
     return this.http.get(`${environment.baseUrl}/users?username=${username}`);
   }
 
-  findPassword(password: string) {
-    return this.http.get(`${environment.baseUrl}/users?password=${password}`);
-  }
-
   login() {
     if (this.form.valid) {
       const payload = this.form.value;
 
     this.findUser(payload.username).subscribe(user => {
-      if(!payload.username == user[0].username) {
-        console.log("Telinha de erro aqui");
-      }
-      this.findPassword(payload.password).subscribe(pass => {
-        if(!payload.password == user[0].password) {
-          console.log("Outra tela de erro aqui");
-        }
+      if(user[0]?.username === undefined || payload.username != user[0]?.username) {
+        this.presentAlert()
+      } else if (user[0]?.password === undefined || payload.password != user[0]?.password) {
+        this.presentAlert()
+      } else {
         this.router.navigate([`guitarras/`]);
-      })
+      }
     });
   }
   }
